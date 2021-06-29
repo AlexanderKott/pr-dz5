7707d1fe3225cf2eacae4890ccb56e394c855df0
AlexanderKott <kotomoto1000@yahoo.com>
2021-06-29 16:40:29 +0200

    dz 6

--- app/src/main/java/dz/mydz/StatesView.kt
+++ app/src/main/java/dz/mydz/StatesView.kt
@@ -1,16 +1,23 @@
 package dz.mydz
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.animation.addListener
 import androidx.core.content.withStyledAttributes
 import kotlin.math.ceil
 import kotlin.math.min
 import kotlin.random.Random
+
 
 class StatesView @JvmOverloads constructor(
     context: Context,
@@ -22,13 +29,11 @@ class StatesView @JvmOverloads constructor(
     private var radius = 0F
 
 
-
-
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
         strokeWidth = lineWidth.toFloat()
         strokeCap = Paint.Cap.ROUND
-       // strokeJoin = Paint.Join.MITER //ROUND
+        // strokeJoin = Paint.Join.MITER //ROUND
     }
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -41,16 +46,65 @@ class StatesView @JvmOverloads constructor(
     var data: Array<Float> = emptyArray<Float>()
         set(value) {
             field = value
-            invalidate()
+            calculatedData = getValues(data)
+            update(aduration.toLong())
         }
 
     private var colors = emptyList<Int>()
+    private var progress = 0F
+    private var valueAnimator: ValueAnimator? = null
+
+    private fun update(aduration: Long) {
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+
+            }
+
+            duration = aduration
+
+            if (animationType == 0) {
+                addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        if (lineCount < 3) {
+                            lineCount++
+                            update(aduration.toLong())
+                            if (lineCount > 0) {
+                                startAngle += calculatedData[lineCount - 1] * 360F
+                            }
+                        }
+
+                    }
+                })
+
+                duration = aduration / 4
+            }
+
+            interpolator = LinearInterpolator()
+            start()
+        }
+
+
+    }
+
+    private var animationType = 0
+    private  var aduration = 0
 
     init {
-        context.withStyledAttributes(attributes, R.styleable.StatesView){
+        context.withStyledAttributes(attributes, R.styleable.StatesView) {
             textPaint.textSize = getDimension(R.styleable.StatesView_textSize, 1F)
             lineWidth = getDimensionPixelSize(R.styleable.StatesView_lineWith, 1)
             paint.strokeWidth = lineWidth.toFloat()
+            aduration = getInt(R.styleable.StatesView_aduration, 1000)
+            animationType = getInteger(R.styleable.StatesView_animType, 0)
+
             colors = listOf(
                 getColor(R.styleable.StatesView_color1, getRandomColor()),
                 getColor(R.styleable.StatesView_color2, getRandomColor()),
@@ -62,37 +116,62 @@ class StatesView @JvmOverloads constructor(
 
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-         radius = min(w, h) / 2F  - lineWidth / 2F
-         center = PointF(w / 2F, h / 2F)
-         circle = RectF(center.x - radius, center.y - radius,
-        center.x + radius, center.y + radius)
+        radius = min(w, h) / 2F - lineWidth / 2F
+        center = PointF(w / 2F, h / 2F)
+        circle = RectF(
+            center.x - radius, center.y - radius,
+            center.x + radius, center.y + radius
+        )
     }
 
 
-
+    private var lineCount = 0
+    private var startAngle = -90F
+    private lateinit var calculatedData: Array<Float>
 
     override fun onDraw(canvas: Canvas) {
-        if (data.isEmpty()){
+        if (data.isEmpty()) {
             return
         }
 
-        var startAngle = -90F
+        if (animationType == 0) {
+            methodTwo(canvas)
+        } else {
+            methodOne(canvas)
+        }
 
-        val calculatedData = getValues(data)
+        firstLineFix(canvas)
+        drawText(canvas)
+    }
 
+    private fun methodOne(canvas: Canvas) {
+        var startAngle3 = -90F
         calculatedData.forEachIndexed { index, it ->
-            val angle =  it * 360F
-            paint.color =  colors.getOrElse(index) { getRandomColor() }
-            canvas.drawArc(circle, startAngle, angle, false,  paint)
-            startAngle += angle
+            val angle = it * 360F
+            paint.color = colors.getOrElse(index) { getRandomColor() }
+            canvas.drawArc(circle, startAngle3, angle * progress, false, paint)
+            startAngle3 += angle
+        }
+    }
+
+    private fun methodTwo(canvas: Canvas) {
+        var startAngle2 = -90F
+
+        if (lineCount > 0) {
+            for (i in 0 until lineCount) {
+                val angle = calculatedData[i] * 360F
+                paint.color = colors.getOrElse(i) { getRandomColor() }
+                canvas.drawArc(circle, startAngle2, angle, false, paint)
+                startAngle2 += angle
+            }
         }
 
-        if (calculatedData[0] > 0) {
-            paint.color = colors.getOrElse(0) { getRandomColor() }
-            startAngle = -90F
-            canvas.drawArc(circle, startAngle, -1F, false, paint)
-        }
+        val angle = calculatedData[lineCount] * 360F
+        paint.color = colors.getOrElse(lineCount) { getRandomColor() }
+        canvas.drawArc(circle, startAngle, angle * progress, false, paint)
+    }
 
+    private fun drawText(canvas: Canvas) {
         canvas.drawText(
             "%.2f%%".format(calculatedData.sum() * 100),
             center.x,
@@ -101,9 +180,16 @@ class StatesView @JvmOverloads constructor(
         )
     }
 
-    private fun View.getInPixels(dp : Int): Int{
-        return  if (dp == 0) 0 else ceil(resources.displayMetrics.density * dp).toInt()
+    private fun firstLineFix(canvas: Canvas) {
+        if (calculatedData[0] > 0) {
+            paint.color = colors.getOrElse(0) { getRandomColor() }
+            canvas.drawArc(circle, -90F, -1F, false, paint)
+        }
     }
 
-    private fun getRandomColor() : Int = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+    private fun View.getInPixels(dp: Int): Int {
+        return if (dp == 0) 0 else ceil(resources.displayMetrics.density * dp).toInt()
+    }
+
+    private fun getRandomColor(): Int = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
 }
